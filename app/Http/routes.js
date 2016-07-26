a2c86bff5e4b76956dd50ddc78270fb53994b8c7
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-26 10:51:25 -0500

    Use database query with "WHERE" statement

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -46,3 +46,24 @@ Route.post('/contact', function * (req, res) {
 
   res.redirect('/contact');
 });
+
+Route.get('/classmates', function * (req, res) {
+  const Database = use('Database');
+  const firstName = req.input('firstName');
+
+  const query = {};
+  if (firstName) {
+    query['first_name'] = firstName;
+  }
+
+  // Get all posts
+  const classmates = yield Database.table('classmates')
+    .where(query).select();
+  // Send to user...
+  res.send(classmates);
+
+  // Not Using Yield
+  // Database.table('classmates').select().then((classmates) => {
+  //   res.send(classmates);
+  // });
+});
