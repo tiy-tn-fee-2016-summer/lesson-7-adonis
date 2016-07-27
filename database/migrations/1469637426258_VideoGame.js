ed8c0c7e7ea042c8efec45cad6dd3e362516fe9f
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-27 11:43:39 -0500

    Update VideoGame Migration

--- database/migrations/1469637426258_VideoGame.js
+++ database/migrations/1469637426258_VideoGame.js
@@ -1,20 +1,26 @@
-'use strict'
+'use strict';
 
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class VideoGameSchema extends Schema {
 
-  up () {
+  up() {
     this.create('video_games', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+      table.increments();
+      table.string('name');
+      table.boolean('arcade');
+      table.integer('player_count');
+
+      // This gives us a field to link to our users table
+      table.integer('user_id');
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('video_games')
+  down() {
+    this.drop('video_games');
   }
 
 }
 
-module.exports = VideoGameSchema
+module.exports = VideoGameSchema;
