2ac6ed548f085764fb25735d790719443f8d877b
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-27 12:12:50 -0500

    Save Game for User, eager load user relation for games.index

--- app/Http/Controllers/GameController.js
+++ app/Http/Controllers/GameController.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const VideoGame = use('App/Model/VideoGame');
+
 class GameController {
 
   * index(request, response) {
-    const games = yield VideoGame.all();
+    const games = yield VideoGame.with('user').fetch();
 
     response.send(games);
   }
@@ -13,7 +15,18 @@ class GameController {
   }
 
   * store(request, response) {
-    //
+    // Grab values from the input
+    const { name, arcade, player_count } = request.all();
+
+    // Get the logged in user for the session
+    const user = yield request.auth.getUser();
+
+    // Save a new game related to our user with values from our input
+    const game = yield user.games().create({ name, arcade, player_count });
+
+    yield request.with({ success: 'Game saved!' }).flash();
+
+    response.redirect('/games');
   }
 
   * show(request, response) {
